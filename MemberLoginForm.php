ba1ccc5bae02bd35bc6ed132be1d0e269b7b2ee4
WebMasterAmy <webmaster@purpledragonwebdevelopment.com>
2013-05-07 21:27:09 -0700

    state information
    beginning to modify state information.  Pausing to get more information
    from client.

--- MemberLoginForm.php
+++ MemberLoginForm.php
@@ -1,8 +1,4 @@
-<?php
-     session_start();
-     $_SESSION = array();
-     session_destroy();
-?>
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -14,9 +10,10 @@
 <body>
 <div id="LoginForm">
 <p>Members of Southern Oregon APICS can log in to the website, if you are a member, contact one of the board members to be added to the site.</p>
-<form action="MemberLogin.php?<?php echo SID; ?>" method="post" name="Login">
+<form action="MemberLogin.php" method="post" name="Login">
 <input id ="email" name="email" type="text" value="email" size="30" maxlength="30" /><br />
 <input id ="password" name="password" type="password" value="password" size="30" maxlength="30" /><br />
+<input type="hidden" name="refer" value="<?php echo $_SESSION['refer']; ?>" />
 <input id ="submit" name="login" type="submit" value="Log In" />
 </form>
 
